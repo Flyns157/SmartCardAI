8229f23f769fc09298de85fe3cfecd42b6ad04a3
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-12-01 10:19:57 +0100

    Refactor training function to use enhanced type checking and modular code structure. Introduce new utility functions for model loading and device management. Add support for resuming training and improve agent initialization logic. Implement a new type checking decorator to enforce parameter types in functions.

--- main.py
+++ main.py
@@ -1,76 +1,90 @@
 import os
 import argparse
 import random
+from pathlib import Path
 
 import torch
 
 import rlcard
-from rlcard.agents import RandomAgent
 from rlcard.utils import (
     set_seed,
     tournament,
     reorganize,
     Logger,
-    plot_curve,
 )
+from utils import (
+    load_model,
+    get_device,
+    plot_curve
+)
+from utils.type_checker import type_check
 
-def get_device():
-    import torch
-    if torch.cuda.is_available():
-        device = torch.cuda.current_device()
-        print("--> Running on the GPU")
-    else:
-        device = "cpu"
-        print("--> Running on the CPU")
 
-    return device 
-
-def train(args):
+@type_check
+def train(seed: str | int | float, env: str, algorithm: str, num_episodes: int, num_eval_games: int, evaluate_every: int, log_dir:  Path | str, learning_rate: float, resume_training: bool, cuda: str, *args, **kwargs):
 
     # Check whether gpu is available
     device = get_device()
 
     # Seed numpy, torch, random
-    set_seed(args.seed)
+    set_seed(seed)
 
     # Make the environment with seed
     env = rlcard.make(
-        args.env,
+        env,
         config={
-            'seed': args.seed,
+            'seed': seed,
         }
     )
 
-    # Initialize the agent and use random agents as opponents
-    if args.algorithm == 'dqn':
-        from rlcard.agents import DQNAgent
-        agent = DQNAgent(
-            num_actions=env.num_actions,
-            state_shape=env.state_shape[0],
-            mlp_layers=[64,64,64,64],
-            device=torch.device(device),
-            learning_rate=args.learning_rate,
-        )
-    elif args.algorithm == 'nfsp':
-        from rlcard.agents import NFSPAgent
-        agent = NFSPAgent(
-            num_actions=env.num_actions,
-            state_shape=env.state_shape[0],
-            hidden_layers_sizes=[64,64],
-            q_mlp_layers=[64,64],
-            device=torch.device(device),
-        )
+    # Charger un modèle existant si possible
+    if resume_training and os.path.exists(model_path := os.path.join(log_dir, 'model.pth')):
+        agent = load_model(model_path=model_path, device=device, env=env)
+    else:
+        print(f"/!\\ No model existing at : {model_path}")
+        print("Start training a new model ...")
+        from time import sleep
+        sleep(3)
+
+        # Initialize the agent and use random agents as opponents
+        match algorithm:
+            case 'dqn':
+                from rlcard.agents import DQNAgent
+                agent = DQNAgent(
+                    num_actions=env.num_actions,
+                    state_shape=env.state_shape[0],
+                    mlp_layers=[64, 64, 64, 64],
+                    device=torch.device(device),
+                    learning_rate=learning_rate,
+                )
+            case 'nfsp':
+                from rlcard.agents import NFSPAgent
+                agent = NFSPAgent(
+                    num_actions=env.num_actions,
+                    state_shape=env.state_shape[0],
+                    hidden_layers_sizes=[64,64],
+                    q_mlp_layers=[64,64],
+                    device=torch.device(device),
+                )
+            case _:
+                raise ValueError(f"Algorithm {algorithm} not supported")
+
     agents = [agent]
-    if args.env == 'uno':
+    if env.name == 'uno':
         from rlcard.models.uno_rule_models import UNORuleAgentV1
         from rule_agents import UNORuleAgentV2, UNORuleAgentV4
+        from rlcard.agents import RandomAgent
         ALL_MODELS = {
+            'rd': RandomAgent,
             'v1': UNORuleAgentV1,
             'v2': UNORuleAgentV2,
             'v4': UNORuleAgentV4,
         }
         for _ in range(1, env.num_players):
-            agents.append(ALL_MODELS[random.choice(list(ALL_MODELS.keys()))]())
+            if (agent_model := random.choice(list(ALL_MODELS.keys()))) not in ('rd'):
+                agents.append(ALL_MODELS[agent_model]())
+            else:
+                agents.append(RandomAgent(num_actions=env.num_actions))
     else:
         for _ in range(1, env.num_players):
             agents.append(RandomAgent(num_actions=env.num_actions))
@@ -79,10 +93,10 @@ def train(args):
     env.set_agents(agents)
 
     # Start training
-    with Logger(args.log_dir) as logger:
-        for episode in range(args.num_episodes):
+    with Logger(log_dir) as logger:
+        for episode in range(num_episodes):
 
-            if args.algorithm == 'nfsp':
+            if algorithm == 'nfsp':
                 agents[0].sample_episode_policy()
 
             # Generate data from the environment
@@ -98,12 +112,12 @@ def train(args):
                 agent.feed(ts)
 
             # Evaluate the performance. Play with random agents.
-            if episode % args.evaluate_every == 0:
+            if episode % evaluate_every == 0:
                 logger.log_performance(
                     episode,
                     tournament(
                         env,
-                        args.num_eval_games,
+                        num_eval_games,
                     )[0]
                 )
 
@@ -111,12 +125,13 @@ def train(args):
         csv_path, fig_path = logger.csv_path, logger.fig_path
 
     # Plot the learning curve
-    plot_curve(csv_path, fig_path, args.algorithm)
+    plot_curve(csv_path, fig_path, algorithm, True)
 
     # Save model
-    save_path = os.path.join(args.log_dir, 'model.pth')
+    save_path = os.path.join(log_dir, 'model.pth')
     torch.save(agent, save_path)
     print('Model saved in', save_path)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("DQN/NFSP example in RLCard")
@@ -180,8 +195,13 @@ if __name__ == '__main__':
         type=float,
         default=5e-5,
     )
+    parser.add_argument(
+        '--resume_training',
+        type=bool,
+        default=False,
+    )
 
     args = parser.parse_args()
 
     os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
-    train(args)
+    train(**dict(args._get_kwargs()))
